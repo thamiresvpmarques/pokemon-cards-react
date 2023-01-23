030e017c4745992977b95313b3815ee996847e42
Thamires Marques <thamiresvieiramarques@gmail.com>
2023-01-23 07:39:02 -0300

    Arrumando o tamanho do componente button.

--- src/components/button/style.js
+++ src/components/button/style.js
@@ -1,11 +1,12 @@
 import styled from "styled-components"
 
 export const BtnPoke = styled.button`
-min-width:100%;
-min-height:100%;
+width:80px;
+height:40px;
 cursor:pointer;
 padding:10px;
 box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
 border-radius: 15px;
 border:none;
+text-align: center;
 `
